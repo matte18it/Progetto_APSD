d0d97a98b71ba6e702550a66273ba4cde83505d7
Pierfancesco <pier.napoli@yahoo.it>
2023-07-12 09:40:41 +0200

    lettura da file

--- Progetto/progettoAPSD.cpp
+++ Progetto/progettoAPSD.cpp
@@ -2,9 +2,13 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <mpi.h>
+#include <fstream>
+#include <iostream>
+using namespace std;
 #define NCOLS 20
 #define NROWS 20
 
+int xPartitions, yPartitions, nThreads, steps;
 
 
 int main(int argc, char *argv[]) {
@@ -12,10 +16,28 @@ int main(int argc, char *argv[]) {
     MPI_Init( &argc, &argv );    
     MPI_Comm_rank( MPI_COMM_WORLD, &rank );    
     MPI_Comm_size( MPI_COMM_WORLD, &size );   
-    printf( "Sono %d di %d\n", rank, size );  
-
+    ifstream configurazione("Configuration.txt");
+    if(configurazione.is_open()){
+        configurazione >> xPartitions >> yPartitions >> nThreads >> steps;
+        configurazione.close();
+    }else{
+        printf("Errore nell'apertura del file di configurazione");
+        exit(1);
+    }
+    ifstream Input("Input.txt");
+    if(configurazione.is_open()){
+        configurazione >> xPartitions >> yPartitions >> nThreads >> steps;
+        configurazione.close();
+    }else{
+        printf("Errore nell'apertura del file di configurazione");
+        exit(1);
+    }
     
-      
+    
+
+
+
+
     MPI_Finalize();  
 
 	
