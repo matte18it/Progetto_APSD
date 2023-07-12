04c0a2911e0813d2c6b6af64518cd5863b7836ed
Pierfancesco <pier.napoli@yahoo.it>
2023-07-12 12:23:45 +0200

    divisione della matrice principale

--- Progetto/progettoAPSD.cpp
+++ Progetto/progettoAPSD.cpp
@@ -8,10 +8,14 @@ using namespace std;
 #define NCOLS 20
 #define NROWS 8
 
+#define v(r,c) ((r)*(NCOLS)+(c))
+MPI_Datatype columnType;
+
 int xPartitions, yPartitions, nThreads, steps;
 int *readM;
 int *writeM;
-int Rank, nProc;
+int *bigM;
+int Rank, nProc, rankUp, rankDown, rankLeft, rankRight;
 void loadConfiguration(){
     ifstream configurazione("Configuration.txt");
     if(configurazione.is_open()){
@@ -23,22 +27,22 @@ void loadConfiguration(){
     }
 }
 void loadBigM(){
-     int *bigM = new int[NCOLS*NROWS];
+        bigM = new int[NCOLS*NROWS];
         ifstream Input("Input.txt");
         if(Input.is_open()){
             char c;
             int i=0;
             while (Input.get(c)){
-                if(c=='\n' ||  c=='\r' || c==' ')
+                if(c!='0' && c!='1')
                     continue;
-                bigM[i]=c-'0';
+                bigM[i]=c - '0';
                 i++;
         } 
             Input.close();
         }else{
             printf("Errore nell'apertura del file di Input");
             exit(1);
-    }
+    } 
 }
 
 void init(){
@@ -46,15 +50,69 @@ void init(){
     if(Rank==0)
         loadBigM();
 }
+void initAutoma(){
+    if(Rank==0){
+        int dest=1;
+        for(int i=0; i<NROWS/yPartitions+2; i++){
+            for(int j=0; j<NCOLS/xPartitions+2; j++){
+                if(i==0 || i==NROWS/yPartitions+1 || j==0 || j==NCOLS/xPartitions+1)
+                    readM[v(i,j)]=0;
+                else
+                    readM[v(i,j)]=bigM[v(i-1,j-1)];
+            }
+        }
+        for(int i=0; i<yPartitions; i++){
+            for(int j=0; j<xPartitions; j++){
+                if(i==0 && j==0){
+                    continue;
+                }else
+                    MPI_Send(&bigM[v(i*(NROWS/yPartitions),j*(NCOLS/xPartitions))], 1, columnType, dest, 0, MPI_COMM_WORLD);
+                dest++;
+                
+            }
+        } 
+        
+
+    }else{
+        for(int i=0; i<NROWS/yPartitions+2; i++){
+            for(int j=0; j<NCOLS/xPartitions+2; j++){
+                readM[v(i,j)]=0;
+            }
+        }
+        MPI_Recv(&readM[v(1,1)], 1, columnType, 0, MPI_ANY_TAG, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+        
+    }
+   
+
+
+}
 
 int main(int argc, char *argv[]) {    
     MPI_Init( &argc, &argv );    
     MPI_Comm_rank( MPI_COMM_WORLD, &Rank );    
-    MPI_Comm_size( MPI_COMM_WORLD, &nProc);  
+    MPI_Comm_size( MPI_COMM_WORLD, &nProc);
+
+    
 
     init();
+
+    MPI_Type_vector(NROWS/yPartitions, NCOLS/xPartitions, (NCOLS/xPartitions)*2, MPI_INT, &columnType);
+    MPI_Type_commit(&columnType);  
+
     readM = new int[(NROWS/yPartitions+2)*(NCOLS/xPartitions+2)];
     writeM = new int[(NROWS/yPartitions+2)*(NCOLS/xPartitions+2)];
+
+    initAutoma();
+    if(Rank==1){
+    
+   
+        for(int i=1; i<NROWS/yPartitions+1; i++){
+            for(int j=1; j<NCOLS/xPartitions+1; j++){
+                printf("%d ", readM[v(i,j)]);
+            }
+            printf("\n");
+        }
+    }
     
     
    
